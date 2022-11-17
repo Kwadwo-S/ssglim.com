1766b934abeaf3e616bde96ae7206458c85def23
Kwadwo-S <83795932+Kwadwo-S@users.noreply.github.com>
2022-11-17 11:26:25 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,204 @@
+let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+document.querySelector('#login-btn').onclick = () =>{
+   loginForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active'); 
+};
+
+document.querySelector('#info-btn').onclick = () =>{
+   contactInfo.classList.add('active');
+}
+
+document.querySelector('#close-contact-info').onclick = () =>{
+   contactInfo.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+   contactInfo.classList.remove('active');
+}
+
+// var swiper = new Swiper(".home-slider", {
+//    loop:true,
+//    grabCursor:true,
+//    navigation: {
+//      nextEl: ".swiper-button-next",
+//      prevEl: ".swiper-button-prev",
+//    },
+// });
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".logo-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      450: {
+         slidesPerView: 2,
+       },
+      640: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1000: {
+        slidesPerView: 5,
+      },
+   },
+});
+
+AOS.init({
+   offset: 400, // offset (in px) from the original trigger point
+   delay: 0, // values from 0 to 3000, with step 50ms
+   duration: 1000 // values from 0 to 3000, with step 50ms
+ });
+
+
+//  Autoslide
+
+const slides=document.querySelector(".slider").children;
+const prev=document.querySelector(".prev");
+const next=document.querySelector(".next");
+const indicator=document.querySelector(".indicator");
+let index=0;
+
+
+  prev.addEventListener("click",function(){
+      prevSlide();
+      updateCircleIndicator(); 
+      resetTimer();
+  })
+
+  next.addEventListener("click",function(){
+     nextSlide(); 
+     updateCircleIndicator();
+     resetTimer();
+     
+  })
+
+  // create circle indicators
+   function circleIndicator(){
+       for(let i=0; i< slides.length; i++){
+          const div=document.createElement("div");
+                div.innerHTML=i+1;
+               div.setAttribute("onclick","indicateSlide(this)")
+               div.id=i;
+               if(i==0){
+                  div.className="active";
+               }
+              indicator.appendChild(div);
+       }
+   }
+   circleIndicator();
+
+   function indicateSlide(element){
+        index=element.id;
+        changeSlide();
+        updateCircleIndicator();
+        resetTimer();
+   }
+    
+   function updateCircleIndicator(){
+      for(let i=0; i<indicator.children.length; i++){
+         indicator.children[i].classList.remove("active");
+      }
+      indicator.children[index].classList.add("active");
+   }
+
+  function prevSlide(){
+      if(index==0){
+         index=slides.length-1;
+      }
+      else{
+         index--;
+      }
+      changeSlide();
+  }
+
+  function nextSlide(){
+     if(index==slides.length-1){
+        index=0;
+     }
+     else{
+        index++;
+     }
+     changeSlide();
+  }
+
+  function changeSlide(){
+            for(let i=0; i<slides.length; i++){
+                slides[i].classList.remove("active");
+            }
+
+      slides[index].classList.add("active");
+  }
+
+  function resetTimer(){
+       // when click to indicator or controls button 
+       // stop timer 
+       clearInterval(timer);
+       // then started again timer
+       timer=setInterval(autoPlay,4000);
+  }
+
+ 
+ function autoPlay(){
+     nextSlide();
+     updateCircleIndicator();
+ }
+
+ let timer=setInterval(autoPlay,4000);
+
+
